07d41a96b601355389fde400d76236feae044932
Margots Kapacs <makapacs@com.com>
2014-07-04 08:38:26 -0500

    adding ui for archive, categories and recent posts

--- templates/blocks/plus/block--ds-recent-posts-sidebar.tpl.php
+++ templates/blocks/plus/block--ds-recent-posts-sidebar.tpl.php
@@ -1,8 +1,10 @@
 <div class="<?php print $context['classes'];?>">
-    <h3 class="widget-title">Recent Posts</h3>
-    <ul class="nav">
-        <li><a href="/">The Emerald City of Oz</a></li>
-        <li><a href="/">Yellow Brick Road</a></li>
-        <li><a href="/">Somewhere Over The Rainbow</a></li>
-    </ul>
+    <?php print render($block_api['contextual_links']);?>
+    <h3 class="widget-title"><?php print $block_api['title']; ?></h3>
+    <?php print $block_api['content']; ?>
+<!--        <ul class="nav">-->
+<!--        <li><a href="/">The Emerald City of Oz</a></li>-->
+<!--        <li><a href="/">Yellow Brick Road</a></li>-->
+<!--        <li><a href="/">Somewhere Over The Rainbow</a></li>-->
+<!--    </ul>-->
 </div>
