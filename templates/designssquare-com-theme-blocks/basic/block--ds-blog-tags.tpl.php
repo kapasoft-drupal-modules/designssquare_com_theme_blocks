4eb2bb105384e51ab289a77472c9e6f0f0bc38b5
Margots Kapacs <makapacs@hotmail.com>
2015-04-27 08:53:11 -0500

    rename path for templates; added Blog Tags and Flex Slide blocks;fix bug to ensure all bloks render no wrap template - v0.0.7

---
+++ templates/designssquare-com-theme-blocks/basic/block--ds-blog-tags.tpl.php
@@ -0,0 +1,45 @@
+<div class="sidebar-widget widget_tag_cloud">
+    <h3 class="sidebar-header">Popular Tags</h3>
+    <ul>
+        <li>
+            <a href="post.html">
+                <span>Mountains</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Winter</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Sports</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Boating</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Recreation</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Tourism</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Nature</span>
+            </a>
+        </li>
+        <li>
+            <a href="post.html">
+                <span>Alps</span>
+            </a>
+        </li>
+    </ul>
+</div>
